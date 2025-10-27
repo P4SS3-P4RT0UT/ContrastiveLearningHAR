cade45716ba36d9656563d87341d2c8572d6693d
P4SS3_P4RT0UT <laberge.la@gmail.com>
2025-10-27 10:29:30 -0400

    test permutation and channel shuffle transf.

--- SimCLR_MotionSense.py
+++ SimCLR_MotionSense.py
@@ -210,24 +210,25 @@ epochs = 200
 temperature = 0.1
 transform_funcs = [
     # transformations.scaling_transform_vectorized, # Use Scaling trasnformation
-    transformations.rotation_transform_vectorized # Use rotation trasnformation
+    # transformations.rotation_transform_vectorized # Use rotation trasnformation
 ]
-transformation_function = simclr_utitlities.generate_composite_transform_function_simple(transform_funcs)
+#transformation_function = simclr_utitlities.generate_composite_transform_function_simple(transform_funcs)
 
 # trasnformation_indices = [2] # Use rotation trasnformation only
 # trasnformation_indices = [1, 2] # Use Scaling and rotation trasnformation
 
-# trasnform_funcs_vectorized = [
-#     transformations.noise_transform_vectorized, 
-#     transformations.scaling_transform_vectorized, 
-#     transformations.rotation_transform_vectorized, 
-#     transformations.negate_transform_vectorized, 
-#     transformations.time_flip_transform_vectorized, 
-#     transformations.time_segment_permutation_transform_improved, 
-#     transformations.time_warp_transform_low_cost, 
-#     transformations.channel_shuffle_transform_vectorized
-# ]
-# transform_funcs_names = ['noised', 'scaled', 'rotated', 'negated', 'time_flipped', 'permuted', 'time_warped', 'channel_shuffled']
+trasnform_funcs_vectorized = [
+    transformations.noise_transform_vectorized, 
+    transformations.scaling_transform_vectorized, 
+    transformations.rotation_transform_vectorized, 
+    transformations.negate_transform_vectorized, 
+    transformations.time_flip_transform_vectorized, 
+    transformations.time_segment_permutation_transform_improved, 
+    transformations.time_warp_transform_low_cost, 
+    transformations.channel_shuffle_transform_vectorized
+ ]
+trasnformation_indices = [5, 7]
+transform_funcs_names = ['noised', 'scaled', 'rotated', 'negated', 'time_flipped', 'permuted', 'time_warped', 'channel_shuffled']
 
 
 
@@ -238,7 +239,7 @@ tf.keras.backend.set_floatx('float32')
 
 lr_decayed_fn = tf.keras.optimizers.schedules.CosineDecay(initial_learning_rate=0.1, decay_steps=decay_steps)
 optimizer = tf.keras.optimizers.SGD(lr_decayed_fn)
-# transformation_function = simclr_utitlities.generate_combined_transform_function(trasnform_funcs_vectorized, indices=trasnformation_indices)
+transformation_function = simclr_utitlities.generate_combined_transform_function(trasnform_funcs_vectorized, indices=trasnformation_indices)
 
 base_model = simclr_models.create_base_model(input_shape, model_name="base_model")
 simclr_model = simclr_models.attach_simclr_head(base_model)
